d36c68f633440eec38b156646b679add5afcc725
kefr kefryus <artemim241@gmail.com>
2026-01-23 12:13:07 +0300

    Sözleşme adresi bağlandı ve bahis butonu hazırlandı

--- lib/client.ts
+++ lib/client.ts
@@ -1,8 +1,5 @@
 import { createThirdwebClient } from "thirdweb";
 
-// BURAYA Thirdweb panelinden aldığın Client ID'yi yapıştır
-const clientId = "9fad833e563385224373381c9943a53a"; 
-
 export const client = createThirdwebClient({
-  clientId: clientId,
+  clientId: "9fad833e563385224373381c9943a53a",
 });
